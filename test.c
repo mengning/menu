e2470d1ed38e943bc1e4dea6581cd99fb43ab418
mengning <mengning@ustc.edu.cn>
2015-02-11 19:55:38 +0800

    add MenuOS logo

--- test.c
+++ test.c
@@ -2,6 +2,120 @@
 #include <stdlib.h>
 #include "menu.h"
 
+#define FONTSIZE 10
+int testMenuOS()
+{
+    int i, j;
+    char data_M[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "  *    *  ",
+        " ***  *** ",
+        " * *  * * ",
+        " * *  * * ",
+        " *  **  * ",
+        " *      * ",
+        " *      * ",
+        " *      * ",
+        "          "
+    };
+    char data_e[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "          ",
+        "    **    ",
+        "   *  *   ",
+        "  *    *  ",
+        "  ******  ",
+        "  *       ",
+        "   *      ",
+        "    ***   ",
+        "          "
+    };
+    char data_n[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "          ",
+        "    **    ",
+        "   *  *   ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "          "
+    };
+    char data_u[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "          ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "  *    *  ",
+        "   *  **  ",
+        "    **  * ",
+        "          "
+    };
+    char data_O[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "   ****   ",
+        "  *    *  ",
+        " *      * ",
+        " *      * ",
+        " *      * ",
+        " *      * ",
+        "  *    *  ",
+        "   ****   ",
+        "          "
+    };
+    char data_S[FONTSIZE][FONTSIZE] =
+    {
+        "          ",
+        "    ****  ",
+        "   **     ",
+        "  **      ",
+        "   ***    ",
+        "     **   ",
+        "      **  ",
+        "     **   ",
+        "  ****    ",
+        "          "
+    };
+
+    for(i=0; i<FONTSIZE; i++)
+    {
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_M[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_e[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_n[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_u[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_O[i][j]);
+        }
+        for(j=0; j<FONTSIZE; j++)
+        {
+            printf("%c", data_S[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
 int Quit(int argc, char *argv[])
 {
     /* add XXX clean ops */
